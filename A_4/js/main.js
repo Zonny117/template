741e1ab1a4d5b22b82adc7fdf3227b458c518037
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2021-09-27 12:12:05 +0900

    a_4 update

---
+++ A_4/js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+
+    $(".menu ul li a").click(function () {
+
+        var cl = $(this).attr("class");
+
+        switch (cl) {
+            case "link1":
+                $(".mMod4").toggleClass("on").siblings().removeClass("on");
+                break;
+            case "link2":
+                $(".mMod7").toggleClass("on").siblings().removeClass("on");
+                break;
+        }
+
+
+        var tf = $(".hidden_wrap>div").is(".on");
+
+        // console.log(tf)
+
+        if (tf) {
+            $(".mSky").addClass("on");
+        } else {
+            $(".mSky").removeClass("on");
+        }
+
+
+
+    })
+
+
+}); ////////////////////////
