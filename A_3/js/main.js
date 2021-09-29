37352454186552bd1088f2e5c59b162dda8e7a66
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2021-09-29 18:38:00 +0900

    test

--- A_3/js/main.js
+++ A_3/js/main.js
@@ -150,15 +150,3 @@ $(function () {
 
 
 }); /////////////////////////////////
-
-if (navigator.userAgent.indexOf('iPhone') != -1) {
-    addEventListener("load", function () {
-        setTimeout(hideURLbar, 0);
-    }, false);
-} else {
-    hideURLbar();
-}
-
-function hideURLbar() {
-    window.scrollTo(0, 1);
-}
