93979b1594d4f2f89e0295efc3d9fb03dcdd31ee
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2021-10-01 15:29:42 +0900

    test

--- A_3_x/js/main.js
+++ A_3_x/js/main.js
@@ -1,14 +1,5 @@
 $(function () {
 
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-    window.addEventListener("resize", () => {
-        console.log("resize");
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty("--vh", `${vh}px`);
-    });
-
     document.getElementsByClassName("vid").controls = false;
 
 
