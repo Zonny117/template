fa90e65001482b9ca1fcfdd2e5a14d2476fc9566
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2021-11-19 10:10:25 +0900

    test

--- A_7/js/main.js
+++ A_7/js/main.js
@@ -1,27 +1,55 @@
 $(function () {
 
 
-    $(".btn_dot").click(function () {
 
-        $(".topmenu").toggleClass("on");
 
+
+
+    $(document).mouseup(function (e) {
+
+        if ($(".topmenu").has(e.target).length === 0) {
+            $(".topmenu").removeClass("on");
+        }
+
+        $(".btn_dot").click(function () {
+
+
+            $(".topmenu").toggleClass("on");
+
+        });
 
     });
 
-    $(document).on("DOMMouseScroll mousewheel", function(){
+
+
+    $(document).on("DOMMouseScroll mousewheel keydown keyup", function () {
 
         let scT = $(window).scrollTop();
 
         let wrap = $("#wrap").height();
         // console.log(scT)
 
-        if(scT >= wrap){
+        if (scT >= wrap) {
             $(".titlebx").addClass("on")
-        }
-        else(
+        } else(
             $(".titlebx").removeClass("on")
         )
     });
+    $("html, body").on("touchmove", function () {
 
+        let touch = $("#wrap").scrollTop();
+
+        let mod2 = $(".mMod2").height();
+
+        //    console.log(touch);
+
+        if (touch >= mod2) {
+            $(".titlebx").addClass("on")
+        } else(
+            $(".titlebx").removeClass("on")
+        )
+
+
+    }); ///////////////
 
 }); ////////////////
