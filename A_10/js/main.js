9eb3931a14fc8a1ffd3d0710f1a10a1ad2b32b3e
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2021-12-06 18:17:20 +0900

    a10 update

---
+++ A_10/js/main.js
@@ -0,0 +1,69 @@
+$(function () {
+
+    let mod4 = $(".mMod4 .tit, .mMod4 .txt").length;
+    let mod7 = $(".mMod7 a").length;
+
+    // console.log(mod4)
+
+    if (mod4 >= 2) {
+        $(".mMod4.forWeb .swiper-slide").wrapInner('<div class="txtbx"></div>');
+    }
+
+    if (mod7 === 0) {
+        $(".mMod10 a").css({
+            marginLeft: 0
+        })
+    }
+
+
+    $(".contact").click(function () {
+
+        $(".mMod6").addClass("on");
+
+        $(this).addClass("on");
+    });
+
+    $(".mMod6 .btn_close").click(function () {
+
+
+        $(".mMod6").removeClass("on");
+
+        $(".contact").removeClass("on");
+
+    });
+
+    $(".btn_dot").click(function () {
+
+
+        $(".topmenu").toggleClass("on");
+
+        $("body").toggleClass("on");
+    });
+
+
+    $(".address").click(function () {
+
+        $(".mMod5").toggleClass("on");
+
+    });
+
+    $(".mag").click(function () {
+
+        $(".lMod9").css({
+            display: "block"
+        });
+
+        $("body").addClass("on");
+    });
+
+    $(".lMod9 .btn_close").click(function () {
+
+        $(".lMod9").css({
+            display: "none"
+        });
+
+        $("body").removeClass("on");
+
+
+    });
+})
