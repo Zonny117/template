56d0578384c990e572193ce2502ac6f73ece9ae4
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2021-12-07 11:55:41 +0900

    test

--- A_10/js/main.js
+++ A_10/js/main.js
@@ -38,6 +38,10 @@ $(function () {
         $(".topmenu").toggleClass("on");
 
         $("body").toggleClass("on");
+
+        $("#wrap").toggleClass("on");
+
+        $(".gnb").toggleClass("on");
     });
 
 
