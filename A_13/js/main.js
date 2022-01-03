11bd8de369eee729f83dd111b6f67123b7d6efa3
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-01-03 17:44:30 +0900

    fixed

--- A_13/js/main.js
+++ A_13/js/main.js
@@ -1,5 +1,89 @@
 $(function () {
 
+
+    let winw = $(window).innerWidth();
+
+
+    //모듈 제어
+    let maintitle = $(".maintitlebx .dn").filter(function () {
+        return $(this).css("display") === "none"
+    });
+
+    if (maintitle.length === 3) {
+        $(".mMod2").css({
+            paddingBottom: 0,
+            height: 0
+        });
+
+        $(".mainbx .ani").css({
+            display: "none"
+        })
+    }
+
+
+    let bg = $(".background").css("display");
+
+    if (bg === "none" && maintitle.length !== 3) {
+        $(".default").css({
+            display: "block"
+        });
+    }
+
+
+    let topmenu = $(".topmenu .dn").filter(function () {
+        return $(this).css("display") === "none"
+    });
+
+    if (topmenu.length === 2) {
+        $(".topmenu, .btn_dot").css({
+            display: "none"
+        });
+    }
+
+    let blue = $(".mMod6, .mMod9").filter(function () {
+        return $(this).css("display") === "none"
+    });
+
+    if (blue.length === 2) {
+        $(".blue").css({
+            display: "none"
+        });
+    }
+
+
+    let mod9 = $(".mMod9").css("display");
+
+    if (mod9 === "none") {
+        $(".blue").css({
+            overflow: "hidden"
+        });
+    }
+
+    let ani = $(".mMod8, .mMod5, footer").filter(function () {
+        return $(this).css("display") === "none"
+    });
+
+    if (ani.length === 3) {
+        $(".mMod4").css({
+            overflow: "hidden"
+        });
+    }
+
+    let footer = $("footer .dn").filter(function () {
+        return $(this).css("display") === "none"
+    });
+
+    if (footer.length === 3) {
+        $("footer").css({
+            display: "none"
+        });
+    }
+
+
+
+
+
+
     let mod6 = $(".mMod6").css("display");
     let tel = $(".mMod6").outerHeight(true);
 
@@ -26,11 +110,23 @@ $(function () {
     function mod9pt_mobile() {
         let height = tel - 60;
 
-        if (mod6 !== "none") {
+        if (mod9 === "none") {
+            $(".blue").css({
+                paddingTop: tel + "px"
+            });
+
+            $(".blue .ani").css({
+                display: "none"
+            });
+        } //////
+        else if (mod6 !== "none") {
             $(".mMod9").css({
                 paddingTop: (80 + height) + "px"
             })
-        } else(
+
+
+        } ////
+        else(
             $(".mMod9").css({
                 paddingTop: "139px"
             })
@@ -38,7 +134,6 @@ $(function () {
     }
 
 
-    let winw = $(window).innerWidth();
 
     if (winw <= 850) {
         mod9pt_mobile();
@@ -46,6 +141,8 @@ $(function () {
         $(window).resize(function () {
             mod9pt_mobile();
         })
+
+        gnb();
     } else {
         mod9pt();
 
@@ -55,20 +152,75 @@ $(function () {
     }
 
 
-    let sct;
+    function gnb() {
+        $(document).on("scroll", function () {
 
-    $(document).on("scroll", function () {
+            let sct = $(window).scrollTop();
 
-        sct = $(window).scrollTop();
+            // console.log(sct);
 
-        // console.log(sct);
+            if (sct >= 50) {
+                $(".gnb").addClass("on");
+            } else {
+                $(".gnb").removeClass("on");
+            }
 
-        if (sct >= 50) {
-            $(".gnb").addClass("on");
-        } else {
-            $(".gnb").removeClass("on");
-        }
+        })
+    }
+
+    $(".btn_dot").click(function () {
+
+        $(".topmenu").css({
+            display: "block"
+        });
+
+        $("html,body").addClass("on");
+
+        $(".ani").addClass("on");
+    })
+
+    $(".btn_close").click(function () {
+        $(".topmenu").css({
+            display: "none"
+        });
+
+        $("html,body").removeClass("on");
+
+        $(".ani").removeClass("on");
 
     })
+
+
+    let iOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+
+    const body = document.querySelector('body');
+    let ios_scroll = 0;
+
+    function enable() {
+        ios_scroll = window.pageYOffset;
+        body.style.overflow = 'hidden';
+        body.style.position = 'fixed';
+        body.style.top = `-${ios_scroll}px`;
+        body.style.width = '100%';
+    }
+
+    function disable() {
+        body.style.removeProperty('overflow');
+        body.style.removeProperty('position');
+        body.style.removeProperty('top');
+        body.style.removeProperty('width');
+        window.scrollTo(0, ios_scroll);
+    }
+
+    if (iOS) {
+        $(".btn_dot").click(function () {
+            enable();
+        })
+
+        $(".btn_close").click(function () {
+            disable();
+        })
+    }
 
 })
