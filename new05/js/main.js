49bba04aaa62a9e90d84266386dd811d1f4e6b26
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-01-10 17:11:30 +0900

    new05

---
+++ new05/js/main.js
@@ -0,0 +1,77 @@
+$(function () {
+
+    function vh() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    vh();
+
+    //모듈 제어
+    let content = $(".content .dn").filter(function () {
+        return $(this).css("display") === "none"
+    });
+
+    if (content.length === 5) {
+        $(".content").css({
+            display: "none"
+        });
+    }
+
+    let footer = $("footer .dn").filter(function () {
+        return $(this).css("display") === "none"
+    });
+
+
+    if (footer.length === 2) {
+        $("footer").css({
+            display: "none"
+        });
+    }
+
+
+
+    $(".mMod9 .swiper-slide").click(function () {
+        $(".lMod9, html, body").addClass("on");
+
+    });
+    $(".btn_close").click(function () {
+        $(".lMod9, html, body").removeClass("on");
+
+    });
+
+
+    let iOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+
+    const body = document.querySelector('body');
+    let ios_scroll = 0;
+
+    function enable() {
+        ios_scroll = window.pageYOffset;
+        body.style.overflow = 'hidden';
+        body.style.position = 'fixed';
+        body.style.top = `-${ios_scroll}px`;
+        body.style.width = '100%';
+    }
+
+    function disable() {
+        body.style.removeProperty('overflow');
+        body.style.removeProperty('position');
+        body.style.removeProperty('top');
+        body.style.removeProperty('width');
+        window.scrollTo(0, ios_scroll);
+    }
+
+    if (iOS) {
+        $(".mMod9 .swiper-slide").click(function () {
+            enable();
+        });
+
+        $(".btn_close").click(function () {
+            disable();
+        })
+
+
+    }
+})
