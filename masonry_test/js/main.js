ee71c4b79c56921905a63c95f690ec70124d64a6
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-02-15 17:21:14 +0900

    qr test

--- masonry_test/js/main.js
+++ masonry_test/js/main.js
@@ -25,9 +25,32 @@ $(function () {
         });
     });
 
-    $(".btn_close").click(function () {
+    $(".lMod9 .btn_close").click(function () {
         $(".lMod9, html, body").removeClass("on");
     });
 
 
+    var qrcode = new QRCode(document.getElementById("qr"), {
+        text: window.location.href,
+        // width: 400,
+        // height: 400,
+        colorDark: "#000",
+        colorLight: "#fff",
+        correctLevel: QRCode.CorrectLevel.H
+    });
+
+    $(window).load(function () {
+
+        let qr = $("#qr img").attr('src');
+
+        $(".qrdown").attr('href', qr);
+    });
+
+    $(".qrcode").click(function () {
+        $(".qrbx").addClass("on");
+    });
+
+    $(".qrbx .btn_close").click(function () {
+        $(".qrbx").removeClass("on");
+    });
 });
