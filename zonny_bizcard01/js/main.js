ca3e6a055263fe2f569f5eab084c715b6c87da01
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-02-23 19:13:44 +0900

    update

--- zonny_bizcard01/js/main.js
+++ zonny_bizcard01/js/main.js
@@ -1,5 +1,16 @@
 $(function () {
 
+    var msnry = new Masonry('.mMod9', {
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        gutter: 5,
+        percentPosition: true,
+    });
+
+    imagesLoaded('.mMod9').on('progress', function () {
+        msnry.layout();
+    });
+
     var swiper = new Swiper(".mainslide", {
         grabCursor: true,
     });
