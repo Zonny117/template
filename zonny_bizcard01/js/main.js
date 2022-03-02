85d3b323776723d5710d98f9197b669dd0e50379
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-03-02 09:58:41 +0900

    update

--- zonny_bizcard01/js/main.js
+++ zonny_bizcard01/js/main.js
@@ -2,32 +2,66 @@ $(function () {
 
 
     var main = new Swiper(".mainslide", {
-        effect: "creative",
-        creativeEffect: {
-            prev: {
-                shadow: true,
-                translate: [0, 0, -400],
-            },
-            next: {
-                translate: ["100%", 0, 0],
-            },
-        },
-        allowTouchMove: false
+        allowTouchMove: false,
     });
 
-    let idx = main.activeIndex;
+    let prot = 0;
 
 
+    $(".mMod4 .swiper-slide").each(function (index) {
+        $(".tit", this).prepend("<span class='number'>" + (index + 1) + "</span>")
+    });
 
     $(".bottom ul li").click(function () {
 
+        if (prot) return;
+        prot = 1;
+        setTimeout(function () {
+            prot = 0;
+        }, 1000);
+
         let idx = $(this).index();
         // console.log(idx);
+
+        let topidx = $(".top li").eq(idx);
+
+
+
 
         $(this).find('a').addClass("on").parents("li").siblings().find('a').removeClass("on");
 
         main.slideTo(idx, 1000);
 
+        topidx.addClass("on").siblings().removeClass("on, first").find("span").removeAttr("style").stop();
+
+        if (topidx.hasClass("on")) {
+            topidx.find('span').each(function (index) {
+                $(this).delay(index * 100).animate({
+                    transform: "translateY(0%)"
+                }, 800, 'easeOutBack')
+            });
+        }
+
+        if ($(".text").hasClass("on")) {
+            setTimeout(function () {
+                $(".mMod4 .swiper-slide").each(function (index) {
+                    $(this).delay(index * 100).animate({
+                        transform: "translateX(0%)",
+                        opacity: 1
+                    }, 1500, 'easeOutCubic')
+                });
+            }, 800);
+        }
+
+        if ($(".media").hasClass("on")) {
+            setTimeout(function () {
+                $(".mMod8, .mMod9 .swiper-slide").each(function (index) {
+                    $(this).delay(index * 100).fadeIn(1000)
+                });
+            }, 800);
+        }
     });
+
+
 
 });
