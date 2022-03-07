8a3b0a2659ef7869187389f3d177733c2cedbbc4
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-03-07 16:21:32 +0900

    update

--- zonny_bizcard01/js/main.js
+++ zonny_bizcard01/js/main.js
@@ -9,16 +9,63 @@ $(window).load(function () {
 
 $(function () {
 
+
+    // 사진 확대용 컨테이너 추가
+    let lmod9 = $(".lMod9 .swiper-slide");
+    // console.log(lmod9.length)
+
+    if (lmod9.length >= 1) {
+        lmod9.wrapInner('<div class="swiper-zoom-container"></div>')
+    };
+
+    function vh() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    vh();
+
+    $(window).resize(function () {
+        vh();
+    });
+
     var main = new Swiper(".mainslide", {
         allowTouchMove: false,
     });
 
     let prot = 0;
 
+    $(".mMod9 .swiper-slide").click(function () {
+
+        let idx = $(this).index();
+
+        $(".lMod9").fadeIn(600);
+
+        var lMod9 = new Swiper(".lMod9 .swiper-container", {
+            initialSlide: idx,
+            zoom: {
+                minRatio: 1,
+                maxRatio: 2,
+            },
+            slidesPerView: 1,
+            spaceBetween: 30,
+            observer: true,
+            observeParents: true,
+        });
+    });
+
+    $(".close1").click(function () {
+        $(".lMod9").fadeOut(600);
+    });
+
+
+
 
     $(".mMod4 .swiper-slide").each(function (index) {
         $(".tit", this).prepend("<span class='number'>" + (index + 1) + "</span>")
     });
+
+
 
     $(".bottom ul li").click(function () {
 
@@ -162,6 +209,7 @@ $(function () {
     $(".heart").click(function () {
         $(".qrpopup").addClass("on");
     });
+
     $(".btn_close").click(function () {
 
         $("#qr img").remove();
