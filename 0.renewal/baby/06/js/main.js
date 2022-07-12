d16b4a4db29442f64c59b03898c97186caefd3dd
Zonny117 <87288370+Zonny117@users.noreply.github.com>
2022-07-12 18:47:45 +0900

    update

--- 0.renewal/baby/06/js/main.js
+++ 0.renewal/baby/06/js/main.js
@@ -18,4 +18,39 @@ $(function () {
         },
         autoHeight: true,
     });
+
+
+    $(".mMod7 a").wrapInner("<p>");
+
+    $(".mMod9 .swiper-slide img").on('click', function () {
+
+        $(".lMod9").addClass("on");
+
+        let idx = mMod9.realIndex;
+
+        var lMod9 = new Swiper(".lMod9 .swiper-container", {
+            initialSlide: idx,
+            slidesPerView: 1,
+            spaceBetween: 50,
+        });
+
+        $(".btn_close").on('click', function () {
+            $(".lMod9").removeClass("on");
+            lMod9.destroy();
+        });
+    });
+
+    $(".btn_QR").on('click', function () {
+        $(".qrpopup").addClass('on');
+    });
+    $(".btn_close").on('click', function () {
+        $(".qrpopup").removeClass('on');
+    });
+
+    vh();
+    control.module(".section2");
+    control.popup('body', '.mMod9 .swiper-slide img, .btn_QR', '.btn_close');
+    autoHeight(mMod4);
+    autoHeight(mMod9);
+    resize_mod9txt(false);
 });
